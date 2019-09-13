a2e9defcffd451326c6589b89e4549d90d8dd29a
Jennifer Birsan <jennibirsan@Cornels-MBP.attlocal.net>
2019-09-13 08:52:20 -0500

    updated pages folder

---
+++ client/src/pages/Save.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Jumbotron from "../components/Jumbotron";
+import { Container} from "../components/Grid";
+import SavedResult from "../components/Saved"
+
+class SaveBook extends Component {
+    state = {
+        savedBooks: []
+    };
+
+    //when this component mounts, grab all books that were save to the database 
+    componentDidMount() {
+        API.getBooks()
+            .then(res => this.setState({ savedBooks: res.data }))
+            .catch(err => console.log(err))
+    }
+
+    //function to remove book by id
+    handleDeleteButton = id => {
+        API.deleteBook(id)
+            .then(res => this.componentDidMount())
+            .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <Container fluid className="container">
+                <Jumbotron />
+                <Container>
+                    <SavedResult savedBooks={this.state.savedBooks} handleDeleteButton={this.handleDeleteButton} />
+                </Container>
+            </Container>
+        )
+    }
+}
+
+
+
+export default SaveBook 
---
+++ client/src/pages/Search.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Jumbotron from "../components/Jumbotron";
+import { Container, Row, Col } from "../components/Grid";
+import SearchForm from "../components/Form";
+import SearchResult from "../components/Search"
+
+
+class SearchBooks extends Component {
+    //create state
+    state = {
+        search: "",
+        books: [],
+        error: "",
+        message: ""
+    };
+
+    //function to take value of what enter in the search bar
+    handleInputChange = event => {
+        this.setState({ search: event.target.value })
+    }
+
+    //function to control the submit button of the search form 
+    handleFormSubmit = event => {
+        event.preventDefault();
+        // once it clicks it connects to the google book api with the search value
+        API.getGoogleSearchBooks(this.state.search)
+            .then(res => {
+                if (res.data.items === "error") {
+                    throw new Error(res.data.items);
+                }
+                else {
+                    // store response in a array
+                    let results = res.data.items
+                    //map through the array 
+                    results = results.map(result => {
+                        //store each book information in a new object 
+                        result = {
+                            key: result.id,
+                            id: result.id,
+                            title: result.volumeInfo.title,
+                            author: result.volumeInfo.authors,
+                            description: result.volumeInfo.description,
+                            image: result.volumeInfo.imageLinks.thumbnail,
+                            link: result.volumeInfo.infoLink
+                        }
+                        return result;
+                    })
+                    // reset the sate of the empty books array to the new arrays of objects with properties geting back from the response
+                    this.setState({ books: results, error: "" })
+                }
+            })
+            .catch(err => this.setState({ error: err.items }));
+    }
+
+    handleSavedButton = event => {
+        // console.log(event)
+        event.preventDefault();
+        console.log(this.state.books)
+        let savedBooks = this.state.books.filter(book => book.id === event.target.id)
+        savedBooks = savedBooks[0];
+        API.saveBook(savedBooks)
+            .then(this.setState({ message: alert("Your book is saved") }))
+            .catch(err => console.log(err))
+    }
+    render() {
+        return (
+            <Container fluid>
+                <Jumbotron>
+                    <h1 className="text-white">Find Your Favorite Books with GoogleBook API</h1>
+                </Jumbotron>
+                <Container>
+                    <Row>
+                        <Col size="12">
+                            <SearchForm
+                                handleFormSubmit={this.handleFormSubmit}
+                                handleInputChange={this.handleInputChange}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+                <br></br>
+                <Container>
+                    <SearchResult books={this.state.books} handleSavedButton={this.handleSavedButton} />
+                </Container>
+            </Container>
+        )
+    }
+
+
+}
+
+export default SearchBooks
