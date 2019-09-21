40861e2fb93ff3b5e0a662ca44973b9da1b61cc0
Jennifer Birsan <jennibirsan@dhcp-10-105-233-118.wireless.northwestern.private>
2019-09-21 12:12:06 -0500

    made scheme unique true so no duplicates

--- models/book.js
+++ models/book.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: Array, required: true },
+  title: { type: String, required: true, unique: true },
+  author: { type: Array, required: true, unique: true },
   description: String,
-  image: {type: String, trim: true},
-  link: {type: String, trim: true},
+  image: {type: String, trim: true, unique: true},
+  link: {type: String, trim: true, unique: true},
   date: { type: Date, default: Date.now }
 });
 
